706f3c3600fede61d38f17ab4b1d7273cf9767f0
hatake <83949721+hatake79@users.noreply.github.com>
2026-03-12 16:15:01 +0700

    Delete src/consoleAlert.js

--- src/consoleAlert.js
+++
@@ -1,7 +0,0 @@
-function sendAlert(data) {
-  console.log("🐋 Whale Activity Detected");
-  console.log("Wallet:", data.wallet);
-  console.log("Balance:", data.balance.toString());
-}
-
-module.exports = { sendAlert };
