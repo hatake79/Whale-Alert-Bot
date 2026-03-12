e82cb43fc573607295b71833a3180748a177770b
hatake <83949721+hatake79@users.noreply.github.com>
2026-03-12 16:21:21 +0700

    Update startBot.js

--- scripts/startBot.js
+++ scripts/startBot.js
@@ -1,15 +1,24 @@
+const logger = require("../utils/logger");
 const { detectWhale } = require("../src/whaleDetector");
-const { sendAlert } = require("../alerts/consoleAlert");
+const { sendTelegramAlert } = require("../alerts/telegramAlert");
 
 const wallets = [
   "0x000000000000000000000000000000000000dead"
 ];
 
 async function run() {
+
+  logger.info("Starting Whale Alert Bot");
+
   for (const wallet of wallets) {
+
     const result = await detectWhale(wallet);
-    sendAlert(result);
+
+    logger.info(`Whale detected: ${wallet}`);
+
+    await sendTelegramAlert(result);
   }
+
 }
 
 run();
